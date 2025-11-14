8844ba9f63e3d87e43f6ddd29e2a47dbc9b62c14
Vitor-sr <83246987+Vitor-sr@users.noreply.github.com>
2025-11-13 23:50:53 -0300

    Delete estoque directory

--- estoque/estoque/estoque/src/main/java/com/gerenciamento/estoque/services/UsuarioRepository.java
+++
@@ -1,6 +0,0 @@
-package com.gerenciamento.estoque.services;
-import com.gerenciamento.estoque.Models.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, String> {
-}
--- estoque/src/main/java/com/gerenciamento/estoque/services/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-
-package com.gerenciamento.estoque.services;
-
-import com.gerenciamento.estoque.Models.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto, String>{
-    
-}
